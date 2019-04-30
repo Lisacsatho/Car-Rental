82a66c087f25646a525698407a71753e7b3ea2d1
Rasmus Erik Nilsson <info@rasmusnilsson.se>
2019-04-30 14:44:33 +0200

    Hotfix

--- src/se/hkr/Database/UserDB/MemberDBHandler.java
+++ src/se/hkr/Database/UserDB/MemberDBHandler.java
@@ -12,8 +12,8 @@ public class MemberDBHandler extends UserDBHandler<Member> {
     public void insert(Member model) throws SQLException {
         super.insert(model);
         String insert = String.format("INSERT INTO member VALUES('%s', '%s')",
-                                     model.getDriverLicensNo(),
-                                     model.getSocialSecurityNo());
+                                    model.getDriverLicensNo(),
+                                    model.getSocialSecurityNo());
         try (Statement statement = databaseConnection.getConnection().createStatement()) {
             statement.execute(insert);
         } catch (Exception e) {
