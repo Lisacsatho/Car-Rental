e6c8b6581d9b87d6e6b120f1171ce847edf431e7
Lisa Csatho <Lisacsatho@hotmail.com>
2019-04-30 15:26:51 +0200

    Start with viewCars

--- src/se/hkr/Model/Vehicle/Car.java
+++ src/se/hkr/Model/Vehicle/Car.java
@@ -6,18 +6,6 @@ public class Car extends Vehicle {
     private int suitcases;
     private CarType carType;
 
-    public Car(int id, double basePrice, String brand, String model, int modelYear, String description, int passengerSeats, FuelType fuelType, GearBox gearBox, List<VehicleOption> vehicleOptions, int suitcases, CarType carType) {
-        super(id, basePrice, brand, model, modelYear, description, passengerSeats, fuelType, gearBox, vehicleOptions);
-        this.suitcases = suitcases;
-        this.carType = carType;
-    }
-
-    public Car(int id, double basePrice, String brand, String model, int modelYear, String description, int passengerSeats, FuelType fuelType, GearBox gearBox, int suitcases, CarType carType) {
-        super(id, basePrice, brand, model, modelYear, description, passengerSeats, fuelType, gearBox);
-        this.suitcases = suitcases;
-        this.carType = carType;
-    }
-
     public int getSuitcases() {
         return suitcases;
     }
@@ -33,4 +21,14 @@ public class Car extends Vehicle {
     public void setCarType(CarType carType) {
         this.carType = carType;
     }
+
+    public Car(int id, double basePrice, String description, int passengerSeats, FuelType fuelType, GearBox gearBox, String modelName, int modelyear, VehicleBrand brand, List<VehicleOption> vehicleOptions, int suitcases, CarType carType) {
+        super(id, basePrice, description, passengerSeats, fuelType, gearBox, modelName, modelyear, brand, vehicleOptions);
+        this.suitcases = suitcases;
+        this.carType = carType;
+
+
+    }
+
 }
+
