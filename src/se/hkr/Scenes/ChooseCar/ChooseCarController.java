0f763a3769f9fcd8d33dce87a5efa5f9333487cd
Håkan Reinholdson <hreinholdson@hotmail.com>
2019-05-01 16:27:24 +0200

    Added search by rental date

--- src/se/hkr/Scenes/ChooseCar/ChooseCarController.java
+++ src/se/hkr/Scenes/ChooseCar/ChooseCarController.java
@@ -1,8 +1,28 @@
 package se.hkr.Scenes.ChooseCar;
 
+import javafx.fxml.Initializable;
+import se.hkr.BookingSession;
+import se.hkr.Database.VehicleDB.CarDBHandler;
+import se.hkr.Database.VehicleDB.VehicleDBHandler;
 import se.hkr.Scenes.ReadController;
 
-public class ChooseCarController implements ReadController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChooseCarController implements ReadController, Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+        try(CarDBHandler carDBHandler = new CarDBHandler()){
+            System.out.println(carDBHandler.readAvailableVehicles(BookingSession.getInstance().getBooking().getStartDate(),
+                    BookingSession.getInstance().getBooking().getEndDate()));
+
+        } catch (Exception e){
+          e.printStackTrace();
+        }
+    }
 
     @Override
     public void filter() {
