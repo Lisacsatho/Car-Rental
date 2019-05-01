678193381d1eb39cb4501206d319ca282f45e7a4
Rasmus Erik Nilsson <info@rasmusnilsson.se>
2019-05-01 16:15:16 +0200

    Further developed readAvailableVehicles database method

--- src/se/hkr/Database/VehicleDB/VehicleDBHandler.java
+++ src/se/hkr/Database/VehicleDB/VehicleDBHandler.java
@@ -25,46 +25,14 @@ public abstract class VehicleDBHandler <V extends Vehicle> extends ModelDBHandle
     }
 
     // Using prints to test out the SQL statement, should be returning objects
-    /*
-    *   SQL should look something like the following when done:
-    *   SELECT *
-        FROM
-            vehicle
-        WHERE
-            id NOT IN (SELECT
-                    vehicleId
-                FROM
-                    booking
-                        JOIN
-                    bookinghasvehicle ON booking.id = bookinghasvehicle.bookingId
-                WHERE
-                    (startDate BETWEEN 'start-date' AND 'end-date')
-                        OR (endDate BETWEEN 'start-date' AND 'end-date')
-                        OR (startDate <= 'start-date'
-                        AND endDate >= 'end-date'));
-    *
-    * */
-    public void readAvailableVehicles(Date startDate, Date endDate) {
+    public List<? extends Vehicle> readAvailableVehicles(Date startDate, Date endDate) {
         List<Vehicle> vehicles = new ArrayList<>();
-        try {
-            Statement statement = connection.createStatement();
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try (CarDBHandler dbHandler = new CarDBHandler()) {
+            vehicles.addAll(dbHandler.readAvailableVehicles(startDate, endDate));
+        } catch (Exception e) {
 
-            String nestedQuery = String.format("SELECT vehicleId FROM Booking_has_Vehicle WHERE" +
-                                                    "(startDate BETWEEN '%1$s' AND '%2$s')" +
-                                                    "OR (endDate BETWEEN '%1$s' AND '%2$s')" +
-                                                    "OR (startDate <= '%1$s' AND endDate >= '%2$s')",
-                                                    dateFormat.format(startDate), dateFormat.format(endDate));
-
-            String query = String.format("SELECT * FROM Vehicle WHERE id NOT IN (%s)", nestedQuery);
-            ResultSet set = statement.executeQuery(query);
-
-            while (set.next()) {
-
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
+        return vehicles;
     }
 
     @Override
