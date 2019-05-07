75f6770ee3a58a0fbdccf9e38f17df254b367641
Håkan Reinholdson <hreinholdson@hotmail.com>
2019-05-07 19:00:00 +0200

    View Bookings start.

--- src/se/hkr/Scenes/ViewBookings/ViewBookingsController.java
+++ src/se/hkr/Scenes/ViewBookings/ViewBookingsController.java
@@ -1,8 +1,29 @@
 package se.hkr.Scenes.ViewBookings;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 import se.hkr.Scenes.ReadController;
 
 public class ViewBookingsController implements ReadController {
+
+    @FXML
+    TextArea txtAreaBookingHistory;
+
+    @FXML
+    ComboBox comboJumpTo;
+
+
+    private void setTxtAreaBookingHistory(){
+
+        /*if (BookingDBHandler.getInstance().getBookingList() != null) {
+
+            for (int i = 0; i < BookingDBHandler.getInstance().getBookingList().size(); i++) {
+                if (BookingDBHandler.getInstance().getBookingList().get(i) != null) {
+                    txtAreaBookingHistory.appendText(BookingDBHandler.getInstance().getBookingList().get(i).toString());
+                }*/
+            }
+
 
     @Override
     public void filter() {
