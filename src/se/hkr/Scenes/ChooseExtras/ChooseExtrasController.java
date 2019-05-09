1792ef5579b588638fb808f2ff9e6739756b21f5
Lisa Csatho <Lisacsatho@hotmail.com>
2019-05-09 14:25:10 +0200

    Working on make a booking

--- src/se/hkr/Scenes/ChooseExtras/ChooseExtrasController.java
+++ src/se/hkr/Scenes/ChooseExtras/ChooseExtrasController.java
@@ -1,16 +1,44 @@
 package se.hkr.Scenes.ChooseExtras;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import se.hkr.BookingSession;
+import se.hkr.Model.Vehicle.Car;
+import se.hkr.Model.Vehicle.Vehicle;
+import se.hkr.Model.Vehicle.VehicleOption;
 import se.hkr.Scenes.ReadController;
 
-public class ChooseExtrasController implements ReadController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChooseExtrasController implements ReadController, Initializable {
+
+    @FXML
+    private TableView <VehicleOption> tblOptions;
+
+    @FXML
+    private TableColumn colCar,
+                        colName,
+                        colPrice;
 
     @Override
-    public void filter() {
+    public void initialize(URL location, ResourceBundle resources) {
+        colCar.setCellValueFactory(new PropertyValueFactory<Car,String >("modelName"));
+        colName.setCellValueFactory(new PropertyValueFactory<VehicleOption, String>("name"));
+        colPrice.setCellValueFactory(new PropertyValueFactory<VehicleOption, Double>("price"));
 
+        ObservableList<VehicleOption> data = FXCollections.observableArrayList();
     }
 
     @Override
+    public void filter() {
+    }
+    @Override
     public void search() {
-
     }
 }
