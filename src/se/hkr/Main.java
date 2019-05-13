1449d63e2ef41750026b99d57b9aca8b81210623
Rasmus Erik Nilsson <info@rasmusnilsson.se>
2019-05-13 11:27:20 +0200

    Added method for resetting filters and keeping the placeholder text

--- src/se/hkr/Main.java
+++ src/se/hkr/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Navigator.getInstance().setPrimaryStage(primaryStage);
-        Navigator.getInstance().navigateTo("ViewBookings/ViewBookingsView.fxml");
+        Navigator.getInstance().navigateTo("ViewCars/ViewCarsView.fxml");
         primaryStage.show();
     }
 
