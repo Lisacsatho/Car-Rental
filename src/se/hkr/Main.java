ab7391388cdc57c5b0abbed2ac8fe1cb80167006
Rasmus Erik Nilsson <info@rasmusnilsson.se>
2019-05-15 15:28:38 +0200

    Nothing

--- src/se/hkr/Main.java
+++ src/se/hkr/Main.java
@@ -12,12 +12,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Navigator.getInstance().setPrimaryStage(primaryStage);
-
-
         Navigator.getInstance().navigateTo("MainMenu/MainMenuView.fxml");
-
         primaryStage.show();
-
     }
 
     public static void main(String[] args) {
